fde21a8ad556a2a442bcf5d56fa42a26aa6a734c
tewsam <bereketytes@gmail.com>
2023-04-15 22:35:18 -0500

    added last change

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,4 @@ def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print('Done x1.1 ayoka nayna ')
+    print('Done x1.1 shetati ferah shermuta ')
